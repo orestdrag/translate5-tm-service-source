455b13f70256ea748cb3992c88bc76d57148a817
orestdrag <orestdrag@gmail.com>
2022-10-20 09:52:21 +0300

    * cleanup

--- build/OtmMemoryServiceConfig.h
+++
@@ -1,4 +0,0 @@
-// the configured options and settings for Tutorial
-#define OtmMemoryService_VERSION_MAJOR 
-#define OtmMemoryService_VERSION_MINOR 
-#define OtmMemoryService_VERSION_PATCH 
--- build/generated/git_version.cpp
+++
@@ -1,6 +0,0 @@
-#include "git_version.h"
-
-const char *gitHash = "6b4cf2a * Fixed cmake lists to build proxygen";
-//const char *gitHashFull = "";
-const char *buildDate="2022/08/15  11:34:39";
-const char *appVersion="0.3.1";
