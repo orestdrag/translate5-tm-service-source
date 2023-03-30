a1a713f30c60b994decdff8bd3c487bbc5fa5ecf
orestdrag <orestdrag@gmail.com>
2023-03-30 16:01:35 +0300

    + added classes to handle different endpoints

--- source/opentm2/core/requestdata.cpp
+++ source/opentm2/core/requestdata.cpp
@@ -47,63 +47,86 @@ int RequestData::buildErrorReturn
   T5LOG(T5ERROR) << errorMsg << ", ErrorCode = " << iRC;
   return( 0 );
 }
+int CreateMemRequestData::checkData(){
+    if ( !_rest_rc_ && strMemName.empty() )
+    {
+        _rc_ = ERROR_INPUT_PARMS_INVALID;
+        buildErrorReturn( _rc_, "Error: Missing memory name input parameter" );
+        _rest_rc_ = BAD_REQUEST ;
+        return _rest_rc_;
+    } /* end */
+    if ( !_rest_rc_ && strSrcLang.empty() )
+    {
+        _rc_ = ERROR_INPUT_PARMS_INVALID;
+        buildErrorReturn( _rc_, "Error: Missing source language input parameter" );
+        _rest_rc_= BAD_REQUEST ;
+        return _rest_rc_;
+    } /* end */
+    if(!_rest_rc_ ){
+        // convert the ISO source language to a OpenTM2 source language name
+        char szOtmSourceLang[40];
+        szOtmSourceLang[0] = 0;//terminate to avoid garbage
+        bool fPrefered = false;
+        EqfGetOpenTM2Lang( NULL, (PSZ)strSrcLang.c_str(), szOtmSourceLang, &fPrefered );
+        
+        if ( szOtmSourceLang[0] == 0 )
+        {
+            _rc_ = ERROR_INPUT_PARMS_INVALID;
+            std::string err =  "Error: Could not convert language " + strSrcLang + "to OpenTM2 language name";
+            buildErrorReturn( _rc_, (PSZ)err.c_str() );
+            _rest_rc_ =  BAD_REQUEST ;
+            return _rest_rc_;
+        } /* end */
+    }
+    fValid = !_rest_rc_;
+    return _rest_rc_;
+}
 
-
-CreateMemRequestData::CreateMemRequestData(std::string inputJson){
+int CreateMemRequestData::parseJSON(){
 // parse input parameters
   //std::string strData, strName, strSourceLang, strDescription;
-  int iRC = 0;
   JSONFactory *factory = JSONFactory::getInstance();
-  void *parseHandle = factory->parseJSONStart( inputJson, &iRC );
+  void *parseHandle = factory->parseJSONStart( strBody, &_rc_ );
   if ( parseHandle == NULL )
   {
-    buildErrorReturn( iRC, "Missing or incorrect JSON data in request body" );
+    buildErrorReturn( _rc_, "Missing or incorrect JSON data in request body" );
     _rest_rc_ =  BAD_REQUEST ;
+    return _rest_rc_;
   } /* end */
-
-  std::string name;
-  std::string value;
-  while ( iRC == 0 )
-  {
-    iRC = factory->parseJSONGetNext( parseHandle, name, value );
-    if ( iRC == 0 )
-    {      
-      if ( strcasecmp( name.c_str(), "data" ) == 0 )
-      {
-        strMemB64EncodedData = value;
-        T5LOG( T5DEBUG) << "JSON parsed name = " << name << "; size = " << value.size();
-      }
-      else{
-        T5LOG( T5DEBUG) << "JSON parsed name = " << name << "; value = " << value;
-        if ( strcasecmp( name.c_str(), "name" ) == 0 )
-        {
-          strMemName = value;
+  //if(_rest_rc_){
+    std::string name;
+    std::string value;
+    while ( _rc_ == 0 )
+    {
+        _rc_ = factory->parseJSONGetNext( parseHandle, name, value );
+        if ( _rc_ == 0 )
+        {      
+            if ( strcasecmp( name.c_str(), "data" ) == 0 )
+            {
+            strMemB64EncodedData = value;
+            T5LOG( T5DEBUG) << "JSON parsed name = " << name << "; size = " << value.size();
+            }
+            else{
+            T5LOG( T5DEBUG) << "JSON parsed name = " << name << "; value = " << value;
+            if ( strcasecmp( name.c_str(), "name" ) == 0 )
+            {
+                strMemName = value;
+            }
+            else if ( strcasecmp( name.c_str(), "sourceLang" ) == 0 )
+            {
+                strSrcLang = value;
+            }else if(strcasecmp(name.c_str(), "loggingThreshold") == 0){
+                int loggingThreshold = std::stoi(value);
+                T5LOG( T5WARNING) <<"OtmMemoryServiceWorker::import::set new threshold for logging" <<loggingThreshold ;
+                T5Logger::GetInstance()->SetLogLevel(loggingThreshold);        
+            }else{
+                T5LOG( T5WARNING) << "JSON parsed unused data: name = " << name << "; value = " << value;
+            }
+            }
         }
-        else if ( strcasecmp( name.c_str(), "sourceLang" ) == 0 )
-        {
-          strSrcLang = value;
-        }else if(strcasecmp(name.c_str(), "loggingThreshold") == 0){
-          int loggingThreshold = std::stoi(value);
-          T5LOG( T5WARNING) <<"OtmMemoryServiceWorker::import::set new threshold for logging" <<loggingThreshold ;
-          T5Logger::GetInstance()->SetLogLevel(loggingThreshold);        
-        }else{
-          T5LOG( T5WARNING) << "JSON parsed unused data: name = " << name << "; value = " << value;
-        }
-      }
-    }
-  } /* endwhile */
-  factory->parseJSONStop( parseHandle );
-
-  if ( !_rest_rc_ && strMemName.empty() )
-  {
-    iRC = ERROR_INPUT_PARMS_INVALID;
-    buildErrorReturn( iRC, "Error: Missing memory name input parameter" );
-    _rest_rc_ = BAD_REQUEST ;
-  } /* end */
-  if ( !_rest_rc_ && strSrcLang.empty() )
-  {
-    iRC = ERROR_INPUT_PARMS_INVALID;
-    buildErrorReturn( iRC, "Error: Missing source language input parameter" );
-    _rest_rc_= BAD_REQUEST ;
-  } /* end */
+    } /* endwhile */
+    factory->parseJSONStop( parseHandle );
+  //}
+  
+  return _rest_rc_;
 }
