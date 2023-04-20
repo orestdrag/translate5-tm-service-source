fae2654de734da518abe1ef6a2527e75f8694e46
orestdrag <orestdrag@gmail.com>
2023-04-20 15:10:43 +0300

    * refactored proxygenhandler -removed dublicating fields, replaced fields with reference to RequestData class * implemented dummy RequestData classes for every supported request

--- include/requestdata.h
+++ include/requestdata.h
@@ -7,6 +7,30 @@
 
 #include "lowlevelotmdatastructs.h"
 
+    
+     enum COMMAND {
+        UNKNOWN_COMMAND,
+        LIST_OF_MEMORIES,
+        SAVE_ALL_TM_ON_DISK,
+        SHUTDOWN,    
+        DELETE_MEM,
+        EXPORT_MEM,
+        EXPORT_MEM_INTERNAL_FORMAT,
+        REORGANIZE_MEM,
+        STATUS_MEM,
+        RESOURCE_INFO,
+
+        START_COMMANDS_WITH_BODY,
+        CREATE_MEM = START_COMMANDS_WITH_BODY, 
+        FUZZY,
+        CONCORDANCE,
+        DELETE_ENTRY,
+        UPDATE_ENTRY,
+        TAGREPLACEMENTTEST,
+        IMPORT_MEM,
+        CLONE_TM_LOCALY,
+        //IMPORT_MEM_INTERNAL_FORMAT
+    };
 
 class EqfMemory;
 class JSONFactory;
@@ -24,6 +48,7 @@ public:
     //requests field
     std::string strUrl;
     std::string strBody;
+    std::string requestAcceptHeader;
 
 
     //return fields
@@ -34,7 +59,8 @@ public:
     //timestamp
 
 
-    RequestData(const std::string& json, const std::string& memName): strBody(json), strMemName(memName) {}
+    RequestData(COMMAND cmnd, const std::string& json, const std::string& memName): strBody(json), strMemName(memName), command(cmnd) {}
+    RequestData(COMMAND cmnd): command(cmnd){}
     virtual ~RequestData() = default;
     //std::weak_ptr <EqfMemory> memory;
 
@@ -42,6 +68,8 @@ public:
 
     int buildErrorReturn(int iRC,char *pszErrorMsg);
 
+
+  COMMAND command = UNKNOWN_COMMAND;
     
     int run();
 protected:
@@ -53,11 +81,6 @@ protected:
 
 
 class CreateMemRequestData: public RequestData{
-    protected:
-    int parseJSON() override ;
-    int checkData() override ;
-    int execute() override ;
-
     int importInInternalFomat();
     int createNewEmptyMemory();
 
@@ -68,47 +91,178 @@ class CreateMemRequestData: public RequestData{
     std::string strMemDescription;
     std::string strMemB64EncodedData; // for import in internal format
     
-    CreateMemRequestData(const std::string& json): RequestData(json, ""){}
-    
+    CreateMemRequestData(const std::string& json): RequestData(COMMAND::CREATE_MEM,json, ""){}
+    CreateMemRequestData(): RequestData(COMMAND::CREATE_MEM) {};
+
+protected:
+    int parseJSON() override;
+    int checkData() override ;
+    int execute() override ;
+};
+
+class ListTMRequestData:public RequestData{
+public:
+    ListTMRequestData(): RequestData(COMMAND::LIST_OF_MEMORIES) {};
+protected:
+    int parseJSON() override { return -1;};
+    int checkData() override { return -1;};
+    int execute() override   { return -1;};
+};
+
+class ImportRequestData:public RequestData{
+public:
+    ImportRequestData(): RequestData(COMMAND::IMPORT_MEM) {};
+protected:
+    int parseJSON() override { return -1;};
+    int checkData() override { return -1;};
+    int execute() override   { return -1;};
+};
+
+class DeleteTMRequestData:public RequestData{
+public:
+    DeleteTMRequestData(): RequestData(COMMAND::DELETE_MEM) {};
+protected:
+    int parseJSON() override { return -1;};
+    int checkData() override { return -1;};
+    int execute() override   { return -1;};
+};
+
+
+
+class ShutdownRequestData:public RequestData{
+public:
+    ShutdownRequestData(): RequestData(COMMAND::SHUTDOWN) {};
+protected:
+    int parseJSON() override { return -1;};
+    int checkData() override { return -1;};
+    int execute() override   { return -1;};
+};
+
+class ResourceInfoRequestData:public RequestData{
+public:
+    ResourceInfoRequestData(): RequestData(COMMAND::RESOURCE_INFO) {};
+protected:
+    int parseJSON() override { return -1;};
+    int checkData() override { return -1;};
+    int execute() override   { return -1;};
+};
+
+
+
+class TagRemplacementRequestData:public RequestData{
+public:
+    TagRemplacementRequestData(): RequestData(COMMAND::TAGREPLACEMENTTEST) {};
+protected:
+    int parseJSON() override { return -1;};
+    int checkData() override { return -1;};
+    int execute() override   { return -1;};
+};
+
+
+class CloneTMRequestData:public RequestData{
+public:
+    CloneTMRequestData(): RequestData(COMMAND::CLONE_TM_LOCALY) {};
+protected:
+    int parseJSON() override { return -1;};
+    int checkData() override { return -1;};
+    int execute() override   { return -1;};
+};
+
+class StatusRequestData:public RequestData{
+public:
+    StatusRequestData(): RequestData(COMMAND::STATUS_MEM) {};
+protected:
+    int parseJSON() override { return -1;};
+    int checkData() override { return -1;};
+    int execute() override   { return -1;};
+};
+
+class ReorganizeRequestData:public RequestData{
+public:
+    ReorganizeRequestData(): RequestData(COMMAND::REORGANIZE_MEM) {};
+protected:
+    int parseJSON() override { return -1;};
+    int checkData() override { return -1;};
+    int execute() override   { return -1;};
+};
+
+
+class UnknownRequestData: public RequestData{
+    public: 
+    UnknownRequestData(): RequestData(UNKNOWN_COMMAND){};
+
+    protected:
+    int parseJSON() override {return -1;};
+    int checkData() override {return -1;};
+    int execute()   override {return -1;};
 };
 
 class StatusMemRequestData: public RequestData{
     public:
-    StatusMemRequestData(const std::string& memName): RequestData("", memName){};
+    StatusMemRequestData(const std::string& memName): RequestData(STATUS_MEM, "", memName) {};
+    StatusMemRequestData(): RequestData(STATUS_MEM){};
+    int parseJSON() override {return 0;};
+    int checkData() override {return -1;};
+    int execute()   override {return -1;};
 };
 
 class DeleteMemRequestData: public RequestData{
     public:
-    DeleteMemRequestData(const std::string& memName): RequestData("", memName){};
+    DeleteMemRequestData(const std::string& memName): RequestData(DELETE_MEM,"", memName){};
+    DeleteMemRequestData(): RequestData(DELETE_MEM){};
+
+protected:
+    int parseJSON() override {return 0;}
+    int checkData() override {return -1;};
+    int execute() override   {return -1;};
 };
 
 
 class ConcordanceSearchRequestData: public RequestData{
     public:
-    ConcordanceSearchRequestData(const std::string& json, const std::string& memName): RequestData(json, memName){};
+    ConcordanceSearchRequestData(const std::string& json, const std::string& memName): RequestData(CONCORDANCE, json, memName){};
+    ConcordanceSearchRequestData(): RequestData(CONCORDANCE){};
+protected:
+    int parseJSON() override {return -1;};
+    int checkData() override {return -1;};
+    int execute()   override {return -1;};
 };
 
 
 class FuzzySearchRequestData: public RequestData{    
     public:
-    FuzzySearchRequestData(const std::string& json, const std::string& memName): RequestData(json, memName){};
+    FuzzySearchRequestData(const std::string& json, const std::string& memName): RequestData(FUZZY,json, memName){};
+    FuzzySearchRequestData(): RequestData(FUZZY){};
+
+protected:
+    int parseJSON() override {return -1;};
+    int checkData() override {return -1;};
+    int execute()   override {return -1;};
 };
 
 class DeleteEntryRequestData: public RequestData{
     private:
     
     public:
-    DeleteEntryRequestData(const std::string& json, const std::string& memName): RequestData(json, memName) {};
+    DeleteEntryRequestData(const std::string& json, const std::string& memName): RequestData(DELETE_ENTRY, json, memName) {};
+    DeleteEntryRequestData(): RequestData(DELETE_ENTRY){}
+
+protected:
+    int parseJSON() override {return -1;};
+    int checkData() override {return -1;};
+    int execute() override   {return -1;};
 };
 
 class UpdateEntryRequestData: public RequestData{
     private:
   
     public:
-    UpdateEntryRequestData(const std::string& json, const std::string& memName): RequestData(json, memName) {};
-    int parseJSON() override ;
-    int checkData() override ;
-    int execute() override ;
+    UpdateEntryRequestData(const std::string& json, const std::string& memName): RequestData(UPDATE_ENTRY, json, memName) {};
+    UpdateEntryRequestData(): RequestData(UPDATE_ENTRY){};
+protected:
+    int parseJSON() override {return -1;};
+    int checkData() override {return -1;};
+    int execute() override   {return -1;};
 };
 
 class ExportRequestData: public RequestData{
@@ -116,7 +270,6 @@ class ExportRequestData: public RequestData{
     int ExportZip();
     int ExportTmx();
     
-    std::string requestFormat;//application/xml or application/binary
     FCTDATA fctdata;
     MEM_EXPORT_IDA IDA;         // Pointer to the export IDA
     PROCESSCOMMAREA CommArea;   // ptr to commmunication area
@@ -125,8 +278,12 @@ class ExportRequestData: public RequestData{
     USHORT ExportProc();
     std::string strTempFile;
     public:
-    ExportRequestData(const std::string& format, const std::string& memName): RequestData("", memName),  requestFormat(format){};
-    int parseJSON() override {return 0;};
+
+    std::string requestFormat;//application/xml or application/binary
+    ExportRequestData(const std::string& format, const std::string& memName): RequestData(EXPORT_MEM, "", memName),  requestFormat(format){};
+    ExportRequestData(): RequestData(EXPORT_MEM){};
+protected:
+    int parseJSON() override {return 0;}
     int checkData() override ;
     int execute() override ;
 
--- source/RestAPI/ProxygenHandler.cpp
+++ source/RestAPI/ProxygenHandler.cpp
@@ -32,25 +32,25 @@ ProxygenHandler::ProxygenHandler(ProxygenStats* stats) : stats_(stats) {
   fWriteRequestsAllowed = true;
 }
 
-const std::map<const ProxygenHandler::COMMAND,const char*> CommandToStringsMap {
-        { ProxygenHandler::COMMAND::UNKNOWN_COMMAND, "UNKNOWN_COMMAND" },
-        { ProxygenHandler::COMMAND::LIST_OF_MEMORIES, "LIST_OF_MEMORIES" },
-        { ProxygenHandler::COMMAND::SAVE_ALL_TM_ON_DISK, "SAVE_ALL_TM_ON_DISK" },
-        { ProxygenHandler::COMMAND::SHUTDOWN, "SHUTDOWN" },
-        { ProxygenHandler::COMMAND::DELETE_MEM, "DELETE_MEM" },
-        { ProxygenHandler::COMMAND::EXPORT_MEM, "EXPORT_MEM" },
-        { ProxygenHandler::COMMAND::EXPORT_MEM_INTERNAL_FORMAT, "EXPORT_MEM_INTERNAL_FORMAT" },
-        { ProxygenHandler::COMMAND::STATUS_MEM, "STATUS_MEM" },
-        { ProxygenHandler::COMMAND::RESOURCE_INFO, "RESOURCE_INFO" },
-        { ProxygenHandler::COMMAND::CREATE_MEM, "CREATE_MEM" },
-        { ProxygenHandler::COMMAND::FUZZY, "FUZZY" },
-        { ProxygenHandler::COMMAND::CONCORDANCE, "CONCORDANCE" },
-        { ProxygenHandler::COMMAND::DELETE_ENTRY, "DELETE_ENTRY" },
-        { ProxygenHandler::COMMAND::UPDATE_ENTRY, "UPDATE_ENTRY" },
-        { ProxygenHandler::COMMAND::TAGREPLACEMENTTEST, "TAGREPLACEMENTTEST" } ,
-        { ProxygenHandler::COMMAND::IMPORT_MEM, "IMPORT_MEM" },
-        { ProxygenHandler::COMMAND::REORGANIZE_MEM, "REORGANIZE_MEM" },
-        { ProxygenHandler::COMMAND::CLONE_TM_LOCALY, "CLONE_MEM"}
+const std::map<const COMMAND,const char*> CommandToStringsMap {
+        { UNKNOWN_COMMAND, "UNKNOWN_COMMAND" },
+        { LIST_OF_MEMORIES, "LIST_OF_MEMORIES" },
+        { SAVE_ALL_TM_ON_DISK, "SAVE_ALL_TM_ON_DISK" },
+        { SHUTDOWN, "SHUTDOWN" },
+        { DELETE_MEM, "DELETE_MEM" },
+        { EXPORT_MEM, "EXPORT_MEM" },
+        { EXPORT_MEM_INTERNAL_FORMAT, "EXPORT_MEM_INTERNAL_FORMAT" },
+        { STATUS_MEM, "STATUS_MEM" },
+        { RESOURCE_INFO, "RESOURCE_INFO" },
+        { CREATE_MEM, "CREATE_MEM" },
+        { FUZZY, "FUZZY" },
+        { CONCORDANCE, "CONCORDANCE" },
+        { DELETE_ENTRY, "DELETE_ENTRY" },
+        { UPDATE_ENTRY, "UPDATE_ENTRY" },
+        { TAGREPLACEMENTTEST, "TAGREPLACEMENTTEST" } ,
+        { IMPORT_MEM, "IMPORT_MEM" },
+        { REORGANIZE_MEM, "REORGANIZE_MEM" },
+        { CLONE_TM_LOCALY, "CLONE_MEM"}
     };
 
 
@@ -58,108 +58,108 @@ void ProxygenHandler::onRequest(std::unique_ptr<HTTPMessage> req) noexcept {
   builder = new ResponseBuilder(downstream_);
   auto methodStr = req->getMethodString ();
   auto method = req->getMethod ();
-  reqUrl = req->getURL () ;
+  pRequest->strUrl = req->getURL () ;
   auto path = req->getPath () ;
   auto queryString   = req->getQueryString () ;
   auto headers = req->getHeaders();
   pMemService = OtmMemoryServiceWorker::getInstance();
  
   std::string requestAcceptHeader = headers.getSingleOrEmpty("Accept");
-  if(command == COMMAND::EXPORT_MEM){
+  if(pRequest->command == COMMAND::EXPORT_MEM){
     if(requestAcceptHeader == "application/xml"){
-      command = COMMAND::EXPORT_MEM;   
+      pRequest->command = COMMAND::EXPORT_MEM;   
     }else if(requestAcceptHeader == "application/zip"){
-      command = COMMAND::EXPORT_MEM_INTERNAL_FORMAT;
+      pRequest->command = COMMAND::EXPORT_MEM_INTERNAL_FORMAT;
     }else{
       errorStr = "export format not specified in Accept header";
-      command = COMMAND::UNKNOWN_COMMAND;
+      pRequest->command = COMMAND::UNKNOWN_COMMAND;
     }
   }
 
-  int id = stats_->recordRequest(this->command);
-  T5Logger::GetInstance()->SetLogInfo(this->command);
-  if(CommandToStringsMap.find(this->command) == CommandToStringsMap.end()){
-    T5Logger::GetInstance()->SetLogBuffer(std::string("Error during ") + toStr(this->command) + " request, id = " + toStr(id));
+  pRequest->requestAcceptHeader = requestAcceptHeader;
+
+  int id = stats_->recordRequest(pRequest->command);
+  T5Logger::GetInstance()->SetLogInfo(pRequest->command);
+  if(CommandToStringsMap.find(pRequest->command) == CommandToStringsMap.end()){
+    T5Logger::GetInstance()->SetLogBuffer(std::string("Error during ") + toStr(pRequest->command) + " request, id = " + toStr(id));
   }else{
-    T5Logger::GetInstance()->SetLogBuffer(std::string("Error during ") + CommandToStringsMap.find(this->command)->second + " request, id = " + toStr(id));
+    T5Logger::GetInstance()->SetLogBuffer(std::string("Error during ") + CommandToStringsMap.find(pRequest->command)->second + " request, id = " + toStr(id));
   }
-  if(memName.empty() == false){
-    T5Logger::GetInstance()->AddToLogBuffer(std::string(", for memory \"") + memName + "\"");
+  if(pRequest->strMemName.empty() == false){
+    T5Logger::GetInstance()->AddToLogBuffer(std::string(", for memory \"") + pRequest->strMemName + "\"");
   }
 
-  if(this->command < COMMAND::START_COMMANDS_WITH_BODY ){ // we handle here only requests without body
+  if(pRequest->command < COMMAND::START_COMMANDS_WITH_BODY ){ // we handle here only requests without body
 
-    switch(this->command){
+    switch(pRequest->command){
       case COMMAND::STATUS_MEM:
       {
-        iRC = pMemService->getStatus( memName, strResponseBody );
+        pRequest->_rest_rc_ = pMemService->getStatus( pRequest->strMemName, pRequest->outputMessage );
         break;
       }
         
       case COMMAND::LIST_OF_MEMORIES:
       {
-        int ret = pMemService->list( strResponseBody );
-        iRC = 200;
+        int ret = pMemService->list( pRequest->outputMessage );
+        pRequest->_rest_rc_ = 200;
         break;
       }
       case COMMAND::TAGREPLACEMENTTEST:
       {
-        iRC = 200;
-        pMemService->tagReplacement(strResponseBody, iRC);
+        pRequest->_rest_rc_ = 200;
+        pMemService->tagReplacement(pRequest->outputMessage, pRequest->_rest_rc_);
         break;
       }
       case COMMAND::EXPORT_MEM:
       case COMMAND::EXPORT_MEM_INTERNAL_FORMAT:
       {
-        pRequest = new ExportRequestData(requestAcceptHeader,memName);
-        pRequest->strUrl = reqUrl;
+        //pRequest = new ExportRequestData(requestAcceptHeader,pRequest->strMemName);
+        //pRequest->strUrl = reqUrl;
         pRequest->run();
-        strResponseBody = pRequest->outputMessage;
-        iRC = pRequest->_rest_rc_;
-        //iRC = pMemService->getMem( memName, requestAcceptHeader,  vMemData);
-        //strResponseBody = std::string(vMemData.begin(), vMemData.end());
-        //iRC = 200;
+        //pRequest->_rest_rc_ = pMemService->getMem( pRequest->strMemName, requestAcceptHeader,  vMemData);
+        //pRequest->outputMessage = std::string(vMemData.begin(), vMemData.end());
+        //pRequest->_rest_rc_ = 200;
         break;
       }
 
       case COMMAND::REORGANIZE_MEM:
       {
-        iRC = 500;
-        //strResponseBody = "{\n\t\"msg\": \"endpoint is not implemented\"\n}";
-        iRC = pMemService->reorganizeMem( memName, strResponseBody);
+        pRequest->_rest_rc_ = 500;
+        //pRequest->outputMessage = "{\n\t\"msg\": \"endpoint is not implemented\"\n}";
+        pRequest->_rest_rc_ = pMemService->reorganizeMem( pRequest->strMemName, pRequest->outputMessage);
         break;
       }
       
       case COMMAND::DELETE_MEM:
       {
         if(fWriteRequestsAllowed == false){
-          iRC = 423;
+          pRequest->_rest_rc_ = 423;
           break;
         }
-        iRC = pMemService->deleteMem( memName, strResponseBody );
+        pRequest->_rest_rc_ = pMemService->deleteMem( pRequest->strMemName, pRequest->outputMessage );
         break;
       }
       case COMMAND::SAVE_ALL_TM_ON_DISK:
       {
-        iRC = pMemService->saveAllTmOnDisk( strResponseBody );
-        iRC = 200;
+        pRequest->_rest_rc_ = pMemService->saveAllTmOnDisk( pRequest->outputMessage );
+        pRequest->_rest_rc_ = 200;
         break;
       }
       case COMMAND::RESOURCE_INFO:{
-        iRC = pMemService->resourcesInfo(strResponseBody, *stats_ );
-        iRC = 200;
+        pRequest->_rest_rc_ = pMemService->resourcesInfo(pRequest->outputMessage, *stats_ );
+        pRequest->_rest_rc_ = 200;
         break;
       }
       case COMMAND::SHUTDOWN:
       { 
           fWriteRequestsAllowed = false;
-          iRC = pMemService->saveAllTmOnDisk( strResponseBody );
+          pRequest->_rest_rc_ = pMemService->saveAllTmOnDisk( pRequest->outputMessage );
           //check tms is in import status
           //close log file
-          if(iRC == 200){
+          if(pRequest->_rest_rc_ == 200){
             pMemService->closeAll();
             T5Logger::GetInstance()->LogStop();          
-            iRC = pMemService->shutdownService();
+            pRequest->_rest_rc_ = pMemService->shutdownService();
           }else{
             fWriteRequestsAllowed = true;
           }
@@ -187,89 +187,86 @@ void ProxygenHandler::onBody(std::unique_ptr<folly::IOBuf> body) noexcept {
 }
 
 void ProxygenHandler::onEOM() noexcept {  
-  if(command >= COMMAND::START_COMMANDS_WITH_BODY){
+  if(pRequest->command >= COMMAND::START_COMMANDS_WITH_BODY){
+    if(!pRequest){
 
-    std::string strInData; 
-    //strInData = ss_body.str();
+    }else{ 
+      body_->coalesce();      
+      pRequest->strBody = (char*) body_->data(); 
 
-    body_->coalesce();  
-    strInData = (char*) body_->data(); 
+      //fix garbage in json 
+      size_t json_end = pRequest->strBody.find("\n}") ;
+      if(json_end > 0 && json_end != std::string::npos){
+        pRequest->strBody = pRequest->strBody.substr(0, json_end + 2);
+      }
 
-    //fix garbage in json 
-    size_t json_end = strInData.find("\n}") ;
-    if(json_end > 0 && json_end != std::string::npos){
-      //strInData[json_end + 1] = '\0';
-      strInData = strInData.substr(0, json_end + 2);
+      std::string truncatedInput = pRequest->strBody.size() > 3000 ? pRequest->strBody.substr(0, 3000) : pRequest->strBody;
+      T5Logger::GetInstance()->SetBodyBuffer(", with body = \n\"" + truncatedInput +"\"\n");
+      switch(pRequest->command){
+
+        case COMMAND::UPDATE_ENTRY:
+        case COMMAND::CREATE_MEM:
+        {
+          if(fWriteRequestsAllowed == false){
+            pRequest->_rest_rc_ = 423;
+            break;
+          }
+          pRequest->run();
+          break;
+        }
+        case COMMAND::IMPORT_MEM:
+        {
+          if(fWriteRequestsAllowed == false){
+            pRequest->_rest_rc_ = 423;
+            break;
+          }
+          pRequest->_rest_rc_ = pMemService->import( pRequest->strMemName, pRequest->strBody, pRequest->outputMessage );
+          break; 
+        }
+        case COMMAND::FUZZY:
+        {
+          pRequest->_rest_rc_ = pMemService->search( pRequest->strMemName, pRequest->strBody, pRequest->outputMessage );
+          break;
+        }
+        case COMMAND::CONCORDANCE:
+        {
+          pRequest->_rest_rc_ = pMemService->concordanceSearch( pRequest->strMemName, pRequest->strBody, pRequest->outputMessage );
+          break;
+        }
+        {
+          if(fWriteRequestsAllowed == false){
+            break;
+          }
+          //pRequest = new UpdateEntryRequestData(strInData);
+          //pRequest->strUrl = reqUrl;
+          pRequest->run();
+          //pRequest->_rest_rc_ = pMemService->updateEntry( pRequest->strMemName, strInData,  pRequest->outputMessage );
+          break;
+        }
+        case COMMAND::DELETE_ENTRY:
+        {
+          if(fWriteRequestsAllowed == false){
+            pRequest->_rest_rc_ = 423;
+            break;
+          }
+          pRequest->_rest_rc_ = pMemService->deleteEntry( pRequest->strMemName, pRequest->strBody,  pRequest->outputMessage );
+          break;
+        }
+        case COMMAND::CLONE_TM_LOCALY:{
+          if(fWriteRequestsAllowed == false){
+            pRequest->_rest_rc_ = 423;
+            break;
+          }
+          pRequest->_rest_rc_ = pMemService->cloneTMLocaly(pRequest->strMemName, pRequest->strBody, pRequest->outputMessage);
+          break;
+        }
+        default:
+        {
+          pRequest->_rest_rc_ = 400;
+          break;
+        }
+      }    
     }
-
-    std::string truncatedInput = strInData.size() > 3000 ? strInData.substr(0, 3000) : strInData;
-    T5Logger::GetInstance()->SetBodyBuffer(", with body = \n\"" + truncatedInput +"\"\n");
-    
-    switch(this->command){
-      case COMMAND::CREATE_MEM:
-      {
-        if(fWriteRequestsAllowed == false){
-          iRC = 423;
-          break;
-        }
-        pRequest = new CreateMemRequestData(strInData);
-        pRequest->strUrl = reqUrl;
-        pRequest->run();
-        strResponseBody = pRequest->outputMessage;
-        iRC = pRequest->_rest_rc_;
-        break;
-      }
-      case COMMAND::IMPORT_MEM:
-      {
-        if(fWriteRequestsAllowed == false){
-          iRC = 423;
-          break;
-        }
-        iRC = pMemService->import( memName, strInData, strResponseBody );
-        break; 
-      }
-      case COMMAND::FUZZY:
-      {
-        iRC = pMemService->search( memName, strInData, strResponseBody );
-        break;
-      }
-      case COMMAND::CONCORDANCE:
-      {
-        iRC = pMemService->concordanceSearch( memName, strInData, strResponseBody );
-        break;
-      }
-      case COMMAND::UPDATE_ENTRY:
-      {
-        if(fWriteRequestsAllowed == false){
-          break;
-        }
-        iRC = pMemService->updateEntry( memName, strInData,  strResponseBody );
-        break;
-      }
-      case COMMAND::DELETE_ENTRY:
-      {
-        if(fWriteRequestsAllowed == false){
-          iRC = 423;
-          break;
-        }
-        iRC = pMemService->deleteEntry( memName, strInData,  strResponseBody );
-        break;
-      }
-      case COMMAND::CLONE_TM_LOCALY:{
-        if(fWriteRequestsAllowed == false){
-          iRC = 423;
-          break;
-        }
-        iRC = pMemService->cloneTMLocaly(memName, strInData, strResponseBody);
-        break;
-      }
-      default:
-      {
-        iRC = 400;
-        break;
-      }
-    }    
-    
     sendResponse();
   }
 }
@@ -288,7 +285,7 @@ void ProxygenHandler::onError(ProxygenError /*err*/) noexcept {
 }
 
 void ProxygenHandler::sendResponse()noexcept{
-    switch(iRC){
+    switch(pRequest->_rest_rc_){
       case 200:
       case 201:
       {
@@ -307,7 +304,7 @@ void ProxygenHandler::sendResponse()noexcept{
         break;
       }
     }
-    builder->status(iRC, responseText);
+    builder->status(pRequest->_rest_rc_, responseText);
     if (FLAGS_request_number) {
       builder->header("Request-Number",
                     folly::to<std::string>(stats_->getRequestCount()));
@@ -320,16 +317,16 @@ void ProxygenHandler::sendResponse()noexcept{
     char version[20];
     Properties::GetInstance()->get_value_or_default(KEY_APP_VERSION, version, 20, "");
     builder->header("t5memory-version", version);  
-    if(command == COMMAND::EXPORT_MEM_INTERNAL_FORMAT){
+    if(pRequest->command == COMMAND::EXPORT_MEM_INTERNAL_FORMAT){
       builder->header("Content-Type", "application/zip");
     } else{
       builder->header("Content-Type", "application/json");
     }
     
     //T5LOG( T5DEBUG) <<  ":: command = ", 
-    //            CommandToStringsMap.find(this->command)->second, ", memName = ", memName.c_str());
-    if(strResponseBody.size())
-      builder->body(strResponseBody);
+    //            CommandToStringsMap.find(this->command)->second, ", pRequest->strMemName = ", pRequest->strMemName.c_str());
+    if(pRequest->outputMessage.size())
+      builder->body(pRequest->outputMessage);
     //builder->send();
 
 
